5a6a33876bd75e1344d6bc721a158d29fb1c9cbb
Thinkabout7 <preowei14@gmail.com>
2025-11-17 20:05:21 +0200

    send the new files

--- models/CommentLike.js
+++ models/CommentLike.js
@@ -1,22 +1,44 @@
-// models/CommentLike.js
+//models/Comment.js
 
 import mongoose from "mongoose";
 
-const commentLikeSchema = new mongoose.Schema(
+const commentSchema = new mongoose.Schema(
   {
+    pin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pin",
+      required: true,
+    },
+
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-    comment: {
+
+    text: {
+      type: String,
+      required: true,
+    },
+//COMMENT.JS
+    parentCommentId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Comment",
-      required: true,
+      default: null,
+    },
+
+    replyToUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+
+    likesCount: {
+      type: Number,
+      default: 0,
     },
   },
   { timestamps: true }
 );
 
-commentLikeSchema.index({ user: 1, comment: 1 }, { unique: true });
-export default mongoose.model("CommentLike", commentLikeSchema);
+export default mongoose.model("Comment", commentSchema);
